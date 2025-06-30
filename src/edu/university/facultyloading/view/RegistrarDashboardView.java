cb304f7a3069f3ff388f78924eb0bf9b0042bd55
KuyaJ2015 <iamjames.valmeo@gmail.com>
2025-06-30 11:47:22 +0800

    constructor added

--- src/edu/university/facultyloading/view/RegistrarDashboardView.java
+++ src/edu/university/facultyloading/view/RegistrarDashboardView.java
@@ -6,6 +6,7 @@
 package edu.university.facultyloading.view;
 
 import java.util.List;
+import java.util.Scanner;
 
 import edu.university.facultyloading.util.OutputFormatter;
 
@@ -15,43 +16,63 @@ import edu.university.facultyloading.util.OutputFormatter;
  */
 public class RegistrarDashboardView {
 
-    public void showMenu(){
+    private final Scanner scanner;
+
+    public RegistrarDashboardView(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void showMenu() {
         System.out.println("\n---- Registrar Dashboard----");
         System.out.println("1) View Faculty Load Report");
         System.out.println("2) View Teaching Load");
         System.out.println("3) Approved Teaching Load");
         System.out.println("4) Logout");
 
-        String  choice = new scanner.nextLine();
+        String choice = scanner.nextLine();
 
         switch (choice) {
             case 1:
-            System.out.println();
-            viewFacultyLoadReport;
+                System.out.println();
+                viewFacultyLoadReport();
             case 2:
-            System.out.println();
-            viewTeachingLoad;
+                System.out.println();
+                viewTeachingLoad();
             case 3:
-            System.out.println();
-            approvedTeachingLoad;
+                System.out.println();
+                approvedTeachingLoad();
             case 4:
-            System.out.println();
-            logOut;      
-            break;
+                System.out.println();
+                logOut();
+                break;
             default:
-            System.out.println();
-            System.out.println("Invalid choice please try again.");
+                System.out.println();
+                System.out.println("Invalid choice please try again.");
                 break;
         }
     }
 
     private void viewFacultyLoadReport() {
+        
 
-
-         OutputFormatter.printHeader("Faculty Load Report");
+        OutputFormatter.printHeader("Faculty Load Report");
         // table headers with fixed width
         System.out.printf("%-5s %-25s %-35s%n", "ID", "Name", "Description");
         OutputFormatter.printDivider();
     }
+
+    private void viewTeachingLoad() {
+
+    }
+
+    private void approvedTeachingLoad() {
+
+    }
+
+    private void logOut() {
+        System.out.println("You have been log out. Thank you!");
+    }
+
+
 
 }
