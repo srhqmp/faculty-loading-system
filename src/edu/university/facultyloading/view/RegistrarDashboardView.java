65281df35d23cd143bf5aecb1e417e2808c58ceb
KuyaJ2015 <iamjames.valmeo@gmail.com>
2025-06-30 10:49:45 +0800

    Added Show menu on Registrar view

--- src/edu/university/facultyloading/view/RegistrarDashboardView.java
+++ src/edu/university/facultyloading/view/RegistrarDashboardView.java
@@ -5,10 +5,53 @@
  */
 package edu.university.facultyloading.view;
 
+import java.util.List;
+
+import edu.university.facultyloading.util.OutputFormatter;
+
 /**
  *
  * @author user
  */
 public class RegistrarDashboardView {
-    
+
+    public void showMenu(){
+        System.out.println("\n---- Registrar Dashboard----");
+        System.out.println("1) View Faculty Load Report");
+        System.out.println("2) View Teaching Load");
+        System.out.println("3) Approved Teaching Load");
+        System.out.println("4) Logout");
+
+        String  choice = new scanner.nextLine();
+
+        switch (choice) {
+            case 1:
+            System.out.println();
+            viewFacultyLoadReport;
+            case 2:
+            System.out.println();
+            viewTeachingLoad;
+            case 3:
+            System.out.println();
+            approvedTeachingLoad;
+            case 4:
+            System.out.println();
+            logOut;      
+            break;
+            default:
+            System.out.println();
+            System.out.println("Invalid choice please try again.");
+                break;
+        }
+    }
+
+    private void viewFacultyLoadReport() {
+
+
+         OutputFormatter.printHeader("Faculty Load Report");
+        // table headers with fixed width
+        System.out.printf("%-5s %-25s %-35s%n", "ID", "Name", "Description");
+        OutputFormatter.printDivider();
+    }
+
 }
